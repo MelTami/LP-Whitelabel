23bc54a7f0d08912971acf6105f30afa3dd93f1d
MelTami <melissa.vavassori@gmail.com>
2024-05-21 15:16:30 -0300

    implementation: swiper

--- src/components/Quem Somos/Carrossel/Carrossel.tsx
+++ src/components/Quem Somos/Carrossel/Carrossel.tsx
@@ -1,74 +1,35 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import { Navigation, Pagination, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export function Carrossel() {
-  const [clicked, setClicked] = useState(false);
   return (
-    <section id="default-carousel" className="flex items-center">
-      <div className="flex absolute w-526 items-center justify-between">
-        <div className="rounded-full bg-white w-10 h-10 bg-opacity-60 ml-1">
-          <button
-            type="button"
-            className="border-black border-r-2 border-b-2 p-2 rounded-sm rotate-135 ml-3.5"
-            onClick={() => setClicked(!clicked)}
-          ></button>
-        </div>
-        <div className="rounded-full bg-white w-10 h-10 bg-opacity-60 mr-1">
-          <button
-            type="button"
-            className="border-black border-r-2 border-b-2 p-2 -rotate-45 rounded-sm ml-1.5 "
-            onClick={() => setClicked(!clicked)}
-          ></button>
-        </div>
-      </div>
-      <div
-        id="buttons"
-        className=" flex w-526 flex-col gap-2 items-center overflow-auto"
-      >
-        {clicked && (
-          <>
+    <div>
+      <Swiper>
+        <SwiperSlide>
+          <div>
             <Image
               src="/photovoltaic-panels-green.png"
-              alt="panels"
-              className="transition-transform"
-              width={526}
-              height={150}
+              alt="photovoltaic panels in a green local"
+              width={550}
+              height={414}
             />
-            <div className="flex gap-2">
-              <button
-                className="rounded-full bg-lilac w-4 h-2 transition-transform"
-                onClick={() => setClicked(true)}
-              ></button>
-              <button
-                className="rounded-full bg-lgray w-2 h-2 transition-transform"
-                onClick={() => setClicked(false)}
-              ></button>
-            </div>
-          </>
-        )}
-        {!clicked && (
-          <>
+          </div>
+          <div>
             <Image
               src="/usina-solar.png"
-              alt="usina"
-              width={526}
-              height={50}
-              className="transition-transform"
+              alt="image"
+              width={743}
+              height={418}
             />
-            <div id="buttons" className="flex gap-2">
-              <button
-                className="rounded-full bg-lgray w-2 h-2 transition-transform"
-                onClick={() => setClicked(true)}
-              ></button>
-              <button
-                className="rounded-full bg-lilac w-4 h-2 transition-transform"
-                onClick={() => setClicked(false)}
-              ></button>
-            </div>
-          </>
-        )}
-      </div>
-    </section>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 }
--- src/components/Simulation/Simulation.tsx
+++ src/components/Simulation/Simulation.tsx
@@ -6,7 +6,7 @@ import "./Form/Form.module.css";
 
 export default function Simulation() {
   return (
-    <section className="flex bg-[url('/handsome-young-black-man-using-smartphone-while-relaxing-couch-home.png')] bg-top bg-cover">
+    <section className="flex bg-[url('/handsome-young-black-man-using-smartphone-while-relaxing-couch-home.png')] bg-cover bg-no-repeat">
       <div className="bg-gradient-to-r from-white via-white w-3/4">
         <div className="flex pl-117.5 pt-8 pb-8 bg-gradient-to-r from-white via-white">
           <Form />
